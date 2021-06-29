e95d19650146416d7edd37c87961b832d966d941
ulimit65535 <2510823550@qq.com>
2021-06-29 20:15:05 +0800

    移除instance_ip

--- notify/models.py
+++ notify/models.py
@@ -7,7 +7,6 @@ class CronResult(BaseModel):
     """接收cron脚本执行结果"""
     task_name = models.CharField(max_length=20, verbose_name='任务名称')
     project = models.CharField(max_length=20, default='默认项目', verbose_name='归属项目')
-    instance_ip = models.GenericIPAddressField(verbose_name='实例ip')
     instance_name = models.CharField(max_length=20, verbose_name='实例名称')
     has_notified = models.BooleanField(default=False, verbose_name='是否已通知')
     result = models.BooleanField(blank=False, null=False, verbose_name='执行结果')
